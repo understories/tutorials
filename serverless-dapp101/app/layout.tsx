06eb777ed14099c17908f500dfee925dd7cc0aa5
vrnvrn <vrneth@proton.me>
2026-01-11 13:14:29 +0800

    Update app layout

--- serverless-dapp101/app/layout.tsx
+++ serverless-dapp101/app/layout.tsx
@@ -3,6 +3,7 @@
 import { ProgressProvider } from '../components/ProgressProvider';
 import { GitHubLink } from '../components/GitHubLink';
 import { LearnLink } from '../components/LearnLink';
+import { DictLink } from '../components/DictLink';
 import { HomeLink } from '../components/HomeLink';
 import { SecurityLink } from '../components/SecurityLink';
 import './globals.css';
@@ -16,10 +17,13 @@ export default function WorkshopTutorialLayout({
     <html lang="en">
       <body>
         <ProgressProvider>
-          <HomeLink />
-          <SecurityLink />
-          <LearnLink />
-          <GitHubLink />
+          <nav className="fixed top-4 right-4 z-50 flex items-center gap-3">
+            <HomeLink />
+            <SecurityLink />
+            <LearnLink />
+            <DictLink />
+            <GitHubLink />
+          </nav>
           {children}
         </ProgressProvider>
       </body>
